56c54dda45164e85463fe3a3ce97f31356e2ea36
reza soroush <rezasoroush28@gmail.com>
2025-04-23 22:00:59 +0330

    DesignTime Factory

---
+++ Zuma.Infrastructure/ToDoContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Zuma.Infrastructure.Context;
+
+namespace Infrastructure
+{
+    public class ToDoContextFactory : IDesignTimeDbContextFactory<ToDoContext>
+    {
+        public ToDoContext CreateDbContext(string[] args)
+        {
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../ZumaProj.Api"))
+            //    .AddJsonFile("appsettings.json")
+            //    .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ToDoContext>();
+            var connectionString = "Server=.;Database=Zuma;Trusted_Connection=True;TrustServerCertificate=true";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ToDoContext(optionsBuilder.Options);
+        }
+    }
+}
