9e9125ff9215f0166ec0c69ed5429d91c928f7ef
reza soroush <rezasoroush28@gmail.com>
2025-04-29 14:41:59 +0330

    adding-BotUser-Migration

---
+++ Zuma.Infrastructure/Migrations/20250429111121_Adding-BotUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zuma.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingBotUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BotUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatId = table.Column<long>(type: "bigint", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JoinedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BotUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BotUsers_ChatId",
+                table: "BotUsers",
+                column: "ChatId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BotUsers");
+        }
+    }
+}
